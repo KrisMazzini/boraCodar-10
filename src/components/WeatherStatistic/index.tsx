c0b40e62c7e9f7de871c555681770113b51b232c
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2023-03-14 19:29:09 -0300

    create weather statistic component

---
+++ src/components/WeatherStatistic/index.tsx
@@ -0,0 +1,42 @@
+import waterdrops from '../../assets/white-waterdrops.svg'
+import shower from '../../assets/white-shower.svg'
+import wind from '../../assets/white-wind.svg'
+
+import { Container } from './styles'
+import { Card } from '../Card'
+
+const STATISTICS = {
+  wind: {
+    icon: wind,
+    unity: 'km/h',
+  },
+  humidity: {
+    icon: waterdrops,
+    unity: '%',
+  },
+  rain: {
+    icon: shower,
+    unity: '%',
+  },
+} as const
+
+interface WeatherStatisticProps {
+  type: keyof typeof STATISTICS
+  value: number
+}
+
+export function WeatherStatistic({ type, value }: WeatherStatisticProps) {
+  const { icon, unity } = STATISTICS[type]
+
+  return (
+    <Card>
+      <Container>
+        <img src={icon} alt="" />
+        <p>{type}</p>
+        <p>
+          {value} <span>{unity}</span>
+        </p>
+      </Container>
+    </Card>
+  )
+}
---
+++ src/components/WeatherStatistic/styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 14.7rem;
+
+  padding: 1.2rem 1.6rem;
+  border-radius: 6px;
+
+  display: grid;
+  grid-template-areas: 'icon type' 'icon value';
+  align-items: center;
+
+  background-color: ${(props) => props.theme['purple-transparent']};
+
+  img {
+    width: 3.2rem;
+    height: 3.2rem;
+
+    grid-area: icon;
+    opacity: 0.4;
+  }
+
+  p:first-of-type {
+    grid-area: type;
+
+    font-size: 1.2rem;
+    line-height: 1.4rem;
+    font-weight: 400;
+
+    text-transform: capitalize;
+
+    color: ${(props) => props.theme['gray-100']};
+  }
+
+  p:last-of-type {
+    grid-area: value;
+
+    font-size: 1.8rem;
+    line-height: 2.2rem;
+
+    color: ${(props) => props.theme['gray-100']};
+
+    span {
+      font-size: 1.4rem;
+      line-height: 1.7rem;
+      color: ${(props) => props.theme['gray-300']};
+    }
+  }
+`
